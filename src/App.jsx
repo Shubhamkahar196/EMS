6c3c4c81c435cdd088b12955d97a8d20a8826f1b
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-15 22:30:23 +0530

    ems project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-xl text-cyan-800'> EMS</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
