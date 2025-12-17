20d0c7082d965537cd8ec87f9d0c1ec07524e834
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-17 10:24:42 +0530

    added localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 import './App.css'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 function App() {
-
+  
+  useEffect(()=>{
+// setLocalStorage()
+getLocalStorage();
+  },[])
 
   return (
     <>
-      {/* <Login/> */}
+      <Login/>
       {/* <EmployeeDashboard/> */}
-      <AdminDashboard/>
+      {/* <AdminDashboard/> */}
     </>
   )
 }
